ebdc4a865c193d37c27f86354f528e626d3b1855
Csaba Barnabas Barcsa <mouseofblind@gmail.com>
2026-03-13 10:12:38 +0100

    Update sys.php
    Added comments to system file.

--- system/sys.php
+++ system/sys.php
@@ -14,13 +14,16 @@ $services = null;
  */
 $configs = null;
 
+/* if file exists, it will gives back an array of services */
 if(file_exists(SUPPORTPATH.DS.'services.php'))
 {
     $services = require_once SUPPORTPATH.DS.'services.php';
 }
+/* if file exists, it will gives back the object of Configs class, that holds several options : ....*/
 if(file_exists(SUPPORTPATH.DS.'configs.php'))
 {
     $configs = require_once SUPPORTPATH.DS.'configs.php';
 }
 
+/* starting the application with a singleton */
 Application::init()->run( $configs, new ServiceContainer($services));
